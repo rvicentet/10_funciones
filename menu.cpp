e57e782bf0792af2f29feaa443c864972936bf03
vicentet <rvicentet@unjbg.edu.pe>
2024-06-24 08:53:39 -0500

    Añadir funciones depositar, retirar y mostrar saldo

--- menu.cpp
+++ menu.cpp
@@ -3,8 +3,16 @@
 #include<iostream>
 using namespace std;
 
+void depositar(float);
+void retirar(float);
+void mostrarSaldo();
+
+float saldo=0; //variable global
+
 int main(){
 	int op;
+	float valor;
+do{
 	cout<<"Cajero autamatico"<<endl;
 	cout<<"1. Depositar"<<endl;
 	cout<<"2. Retirar"<<endl;
@@ -12,6 +20,19 @@ int main(){
 	cout<<"0. Salir"<<endl;
 	cout<<"Elija una opcion: "; cin>>op;
 	switch(op){
+		case 1:
+			cout<<"Ingrese el monto a depositar"<<endl;
+			cin>>valor;
+			depositar(valor);
+			break;
+		case 2:
+			cout<<"Ingrese el monto a retirar"<<endl;
+			cin>>valor;
+			retirar(valor);
+			break;
+		case 3:
+			mostrarSaldo();
+			break;
 		case 0:
 			cout<<"Saliendo . . ."<<endl;
 			system("pause");
@@ -21,6 +42,20 @@ int main(){
 			 system("pause");
 			 break;
 	}
-	
-	return 0;
+}
+while(op != 0);
+
+return 0;
+}
+
+void depositar (float monto){
+	saldo= saldo + monto;
+}
+
+void retirar (float monto){
+	saldo= saldo - monto;
+}
+
+void mostrarSaldo(){
+	cout<<"El saldo actual es: "<<saldo<<endl;
 }
